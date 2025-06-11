68151ba12dd90112a21cfc767fcf8303b24e7cba
Leonid Lovsky <leonid.lovsky@gmail.com>
2025-06-12 00:52:42 +0300

    Add test controllers: HelloController and WelcomeController

--- src/main/java/com/dailyfarm/authservice/AuthServiceApplication.java
+++ src/main/java/com/dailyfarm/authservice/AuthServiceApplication.java
@@ -1,4 +1,4 @@
-package com.dailyfarm.auth;
+package com.dailyfarm.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/dailyfarm/authservice/AuthServiceApplicationTests.java
+++ src/test/java/com/dailyfarm/authservice/AuthServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dailyfarm.auth;
+package com.dailyfarm.authservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
