debcffe0eb9993ae75631d342028f33abbf915d7
Leonid Lovsky <leonid.lovsky@gmail.com>
2025-06-12 00:54:22 +0300

    Add basic user management: entity, repository, service

---
+++ src/main/java/com/dailyfarm/authservice/service/UserService.java
@@ -0,0 +1,27 @@
+package com.dailyfarm.authservice.service;
+
+import com.dailyfarm.authservice.entity.User;
+import com.dailyfarm.authservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
