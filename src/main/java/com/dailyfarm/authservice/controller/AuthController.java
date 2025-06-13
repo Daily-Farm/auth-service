0ea1c1872fc405dcf332a9822906433bf84ec705
Leonid Lovsky <leonid.lovsky@gmail.com>
2025-06-13 22:49:32 +0300

    Add basic login endpoint

--- src/main/java/com/dailyfarm/authservice/controller/AuthController.java
+++ src/main/java/com/dailyfarm/authservice/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.dailyfarm.authservice.controller;
 import com.dailyfarm.authservice.entity.User;
 import com.dailyfarm.authservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +24,13 @@ public class AuthController {
     @PostMapping("/register")
     public User register(@RequestBody User user) {
         return userService.save(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User loginRequest) {
+        return userService.findByEmail(loginRequest.getEmail())
+            .filter(user -> user.getPassword().equals(loginRequest.getPassword()))
+            .map(user -> ResponseEntity.ok("Login successful"))
+            .orElseGet(() -> ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials"));
     }
 }
