fb8f71b8ea12e7ee7317342494bf877ce5b29f29
Leonid Lovsky <leonid.lovsky@gmail.com>
2025-06-13 20:40:29 +0300

    Add basic AuthController with user registration endpoint

---
+++ src/main/java/com/dailyfarm/authservice/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.dailyfarm.authservice.controller;
+
+import com.dailyfarm.authservice.entity.User;
+import com.dailyfarm.authservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    @Autowired
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.save(user);
+    }
+}
